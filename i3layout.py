85ab2d9d4810bad2aeb510573c97ec3363110766
Toma Adrian Ionut <sirhadrian21@gmail.com>
2024-02-21 01:15:34 +0200

    removed border and indicator

--- i3layout.py
+++ i3layout.py
@@ -4,9 +4,6 @@ from i3ipc import Connection, Event
 
 
 def find_parent(i3, window_id):
-    """
-        Find the parent of a given window id
-    """
 
     def finder(con, parent):
         if con.id == window_id:
@@ -21,18 +18,13 @@ def find_parent(i3, window_id):
 
 
 def set_layout(i3, e):
-    """
-        Set the layout/split for the currently
-        focused window to either vertical or
-        horizontal, depending on its width/height
-    """
-    win = i3.get_tree().find_focused()
-    parent = find_parent(i3, win.id)
 
-    if (parent and parent.layout != 'tabbed'
-            and parent.layout != 'stacked'):
+    focused_window = i3.get_tree().find_focused()
+    parent = find_parent(i3, focused_window.id)
 
-        if win.rect.height > win.rect.width:
+    if parent and parent.layout != 'tabbed' and parent.layout != 'stacked':
+
+        if focused_window.rect.height > focused_window.rect.width:
             if parent.orientation == 'horizontal':
                 i3.command('split v')
         else:
